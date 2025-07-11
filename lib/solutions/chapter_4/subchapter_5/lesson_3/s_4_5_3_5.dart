d712fb237e0def0a63418e4afb2406e394d99a6b
Johannes Langbein <jllangbein@gmail.com>
2025-07-11 12:44:55 +0200

    solved 4_5_3_5

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,52 @@ class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ColorfulRectangles();
+  }
+}
+
+class ColorfulRectangles extends StatelessWidget {
+  const ColorfulRectangles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // spacing: 16.0,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: Text('Lösung:', style: TextStyle(color: Colors.black)),
+        ),
+        Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            )
+          ],
+        )
+      ],
+    );
   }
 }
