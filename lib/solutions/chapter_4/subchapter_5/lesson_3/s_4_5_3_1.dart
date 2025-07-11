d789abe782c4c2de26f83da37da93dfc9c8bbb85
Johannes Langbein <jllangbein@gmail.com>
2025-07-11 12:04:23 +0200

    solved 4_5_3_1

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,6 +4,33 @@ class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ColumnAndContainers();
+  }
+}
+
+class ColumnAndContainers extends StatelessWidget {
+  const ColumnAndContainers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.blueGrey,
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue,
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.cyan,
+        ),
+      ],
+    );
   }
 }
