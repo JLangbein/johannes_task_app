339d61f612fe54e24cbb876e1b9be8511d99499f
Johannes Langbein <jllangbein@gmail.com>
2025-07-14 12:23:52 +0200

    solved 4_5_3_6

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,42 @@ class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ProductListView();
+  }
+}
+
+class Product {
+  // attributes
+  String name = '';
+  double price = 0.0;
+
+  // constructor
+  Product({required this.name, required this.price});
+}
+
+List<Product> productList = [
+  Product(name: 'Apple', price: 1.50),
+  Product(name: 'Butter', price: 4.50),
+  Product(name: 'Semiconductor', price: 2500.00),
+  Product(name: 'Fungal Growth', price: 0.05),
+  Product(name: 'Milk', price: 2.00)
+];
+
+class ProductListView extends StatelessWidget {
+  const ProductListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: productList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.shopping_cart),
+          title: Text(productList[index].name),
+          trailing: Text('${productList[index].price.toString()} Eur'),
+        );
+      },
+    );
   }
 }
