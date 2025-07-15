bdd86bcf07b58bb0dd2142a164ef9451ea0e0612
Johannes Langbein <jllangbein@gmail.com>
2025-07-15 11:31:33 +0200

    solved 4_5_2_3

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,37 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNamWidget();
+  }
+}
+
+class MyNamWidget extends StatefulWidget {
+  const MyNamWidget({super.key});
+
+  @override
+  State<MyNamWidget> createState() => _MyNamWidgetState();
+}
+
+class _MyNamWidgetState extends State<MyNamWidget> {
+  // states (attributes)
+  String name = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(name),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  name = 'Rüdiger Nehberg';
+                });
+              },
+              child: Text('Name anzeigen'))
+        ],
+      ),
+    );
   }
 }
