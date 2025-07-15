e38324bfa31df2c9371ec819e827e4de3edb847a
Johannes Langbein <jllangbein@gmail.com>
2025-07-15 11:54:32 +0200

    solved 4_5_2_4

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,47 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  // states (attributes)
+  String name = '';
+  String buttonLabel = 'Name anzeigen';
+  bool shown = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(name),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (shown) {
+                    shown = false;
+                    name = '';
+                    buttonLabel = 'Name anzeigen';
+                  } else {
+                    shown = true;
+                    name = 'Rüdiger Nehberg';
+                    buttonLabel = 'Name verstecken';
+                  }
+                });
+              },
+              child: Text(buttonLabel))
+        ],
+      ),
+    );
   }
 }
